15e9b881ab80815573dc939bcf1404913d5f0dd0
Enrico Weigelt, metux IT consult <info@metux.net>
2025-10-01 14:44:17 +0200

    dix: inline SProcAllocColor()
    Now that we have untwisted Xinerama side, it's trivial to inline
    the few lines for byte-swapping into the actual handlers.
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- dix/dispatch.c
+++ dix/dispatch.c
@@ -2691,6 +2691,13 @@ ProcAllocColor(ClientPtr client)
         return rc;
     }
 
+    if (client->swapped) {
+        swapl(&stuff->cmap);
+        swaps(&stuff->red);
+        swaps(&stuff->green);
+        swaps(&stuff->blue);
+    }
+
     xAllocColorReply rep = {
         .red = stuff->red,
         .green = stuff->green,
